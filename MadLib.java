e812298007f51c6964fef52473fb18726e293521
kevin115533 <kevin115533@yahoo.com>
2018-09-03 16:14:53 -0500

    third

--- MadLib.java
+++ MadLib.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class MadLib {
 	public static void main(String[] args) {
 		
-		int fNum, sNum, tNum, foNum;
+		String fNum;
+		int sNum, tNum, foNum;
 		String word1, word2, word3, word4, word5, word6;
 		float float1, float2;
 		
 		
+		Scanner Input1 = new Scanner (System.in);
+		
+		System.out.print("Enter a number: ");
+		fNum = Input1.nextLine();
+		System.out.print(fNum);
 	}
 }
