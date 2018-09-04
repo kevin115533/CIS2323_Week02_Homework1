74c9cd8dfb56a90fe10cea065afb7a58b502e3c8
kevin115533 <kevin115533@yahoo.com>
2018-09-03 21:46:30 -0500

    fifth

--- MadLib.java
+++ MadLib.java
@@ -11,16 +11,32 @@ public class MadLib {
 		
 		Scanner Input1 = new Scanner (System.in);
 		Scanner Input2 = new Scanner (System.in);
+		Scanner Input3 = new Scanner (System.in);
+		Scanner Input4 = new Scanner (System.in);
+		Scanner Input5 = new Scanner (System.in);
+		Scanner Input6 = new Scanner (System.in);
+		Scanner Input7 = new Scanner (System.in);
+		Scanner Input8 = new Scanner (System.in);
+		Scanner Input9 = new Scanner (System.in);
 		
-		System.out.println("Welcome to the Java Madlib.");
-		System.out.print("Enter a number: ");
+		System.out.println("Java Madlib.");
+		
+		System.out.print("Your first name: ");
+		word2 = Input3.nextLine();
+		
+		System.out.print("Number: ");
 		fNum = Input1.nextInt();
 		
-		System.out.print("Enter a noun: ");
+		System.out.print("Number: ");
+		sNum = Input5.nextInt();
+		
+		System.out.print("Adjective: ");
 		word1 = Input2.nextLine();
 		
-		System.out.println("To get better at programming you must practice " + fNum + " hours a day.");
-		System.out.println("To get better at programming you must practice " + word1 + " hours a day.");
+		System.out.print("Welcome to the intro Java course " + word2 + ". ");
+		System.out.print("To become better at programming, you must practice " + fNum + " hours a day. ");
+		System.out.print("After all this practicing, you might become a " + word1 + " programmer. ");
+		System.out.print("No matter the type of programmer you are, you will still know more about Java than " + sNum + "% of people. ");
 	}
 }
 
